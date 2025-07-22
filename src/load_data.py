1c65d000ea204d2c7fcdfa9dc6c8de5ec60bb9e2
Raajaganeish <raaja.sekar96@gmail.com>
2025-07-22 18:23:09 +0530

    add folder structure and load_data.py

---
+++ src/load_data.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+ 
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+df.to_csv("data/raw/housing.csv", index=False)
+print("Data loaded and saved to data/raw/housing.csv")
