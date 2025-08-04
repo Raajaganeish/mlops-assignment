9e030f4e18bb9b4081d2287370e0cfe3c1b8b158
vardamanengineer <46510733+vardamanengineer@users.noreply.github.com>
2025-08-04 16:38:02 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -141,6 +141,7 @@ def main():
         f"✅ Training completed: model = {best_model_name}, "
         f"version = {model_version}"
     )
+    print("✅ Test logger to trigger re-train pipeline")
 
 
 # Run
